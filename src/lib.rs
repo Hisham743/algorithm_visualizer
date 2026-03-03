6b3690381801081379a500e585d2bd0af4022e16
Hisham Shefeekh <hishamshefeekh@gmail.com>
2026-03-03 23:55:28 +0530

    use a trait instead of a struct for the algorithms

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,44 @@
 #![feature(gen_blocks)]
 
-mod algorithms;
+pub trait Sort<T: Ord + Clone>: AsRef<[T]> + AsMut<[T]> {}
 
-#[derive(Debug)]
-pub struct SortingEngine<T: Ord>(Vec<T>);
+impl<T: Ord + Clone> Sort<T> for [T] {}
 
-impl<T: Ord + Clone> SortingEngine<T> {
-    pub fn new() -> Self {
-        SortingEngine(Vec::new())
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::iter;
+
+    macro_rules! test_algorithm {
+        ($algorithm:ident) => {
+            #[test]
+            fn $algorithm() {
+                let mut numbers = Vec::new();
+
+                numbers.$algorithm().for_each(drop);
+                assert!(numbers.is_sorted(), "empty case");
+
+                numbers.push(fastrand::i32(..));
+                numbers.$algorithm().for_each(drop);
+                assert!(numbers.is_sorted(), "single case");
+
+                numbers = iter::repeat_with(|| fastrand::i32(..)).take(100).collect();
+                numbers.$algorithm().for_each(drop);
+                assert!(numbers.is_sorted(), "100 case");
+
+                numbers.$algorithm().for_each(drop);
+                assert!(numbers.is_sorted(), "sorted case");
+
+                numbers.reverse();
+                numbers.$algorithm().for_each(drop);
+                assert!(numbers.is_sorted(), "reverse case");
+            }
+        };
     }
 
-    pub fn get_elements(&self) -> &[T] {
-        &self.0
-    }
-
-    pub fn set_elements(&mut self, elements: impl IntoIterator<Item = T>) {
-        self.0 = elements.into_iter().collect();
-    }
-
-    pub fn shuffle(&mut self) {
-        fastrand::shuffle(&mut self.0);
-    }
-}
-
-impl<T: Ord + Clone> Default for SortingEngine<T> {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl<T, I> From<I> for SortingEngine<T>
-where
-    T: Ord,
-    I: Into<Vec<T>>,
-{
-    fn from(value: I) -> Self {
-        SortingEngine(value.into())
-    }
+    test_algorithm!(bubble_sort);
+    test_algorithm!(selection_sort);
+    test_algorithm!(insertion_sort);
+    test_algorithm!(merge_sort);
+    test_algorithm!(quick_sort);
 }
