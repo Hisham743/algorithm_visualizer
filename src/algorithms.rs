8bc067d1c900f90c94b2679d62b72edbb483105f
Hisham Shefeekh <hishamshefeekh@gmail.com>
2026-03-03 21:41:55 +0530

    implement bubble sort

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -1,6 +1,25 @@
 use crate::SortingEngine;
 
-impl<T: Ord> SortingEngine<T> {}
+impl<T: Ord> SortingEngine<T> {
+    pub fn bubble_sort(&mut self) -> impl Iterator<Item = usize> {
+        gen {
+            let length = self.0.len();
+            if length < 2 {
+                return;
+            }
+
+            for i in 0..(length - 1) {
+                for j in 0..(length - i - 1) {
+                    yield j;
+
+                    if self.0[j] > self.0[j + 1] {
+                        self.0.swap(j, j + 1);
+                    }
+                }
+            }
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
