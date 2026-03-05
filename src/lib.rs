e8f16acc1c7f50d03b64a63a7b7e64de03ca3724
Hisham Shefeekh <hishamshefeekh@gmail.com>
2026-03-06 02:47:48 +0530

    implement `Display` for `Algorithm`

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![feature(gen_blocks)]
 
+use std::fmt::Display;
+
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Algorithm {
     Bubble,
@@ -7,6 +9,12 @@ pub enum Algorithm {
     Insertion,
     Merge,
     Quick,
+}
+
+impl Display for Algorithm {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?} Sort", &self)
+    }
 }
 
 pub trait Sortable<T: Ord>: AsRef<[T]> + AsMut<[T]> {
