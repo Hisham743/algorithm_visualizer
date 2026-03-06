4f92f2ae5e8fafa05b1a182124b06c218cfcb91c
Hisham Shefeekh <hishamshefeekh@gmail.com>
2026-03-07 01:35:41 +0530

    go back to using `SortEngine` struct instead of `Sortable` trait

---
+++ src/algorithms.rs
@@ -0,0 +1,255 @@
+use std::fmt::Display;
+
+use crate::SortingEngine;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Algorithm {
+    Bubble,
+    Selection,
+    Insertion,
+    Merge,
+    Quick,
+}
+
+impl Display for Algorithm {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?} Sort", &self)
+    }
+}
+
+impl<T, U> SortingEngine<T, U>
+where
+    T: Ord,
+    U: AsRef<[T]> + AsMut<[T]>,
+{
+    pub fn bubble_sort(&mut self) -> impl Iterator<Item = usize> {
+        gen move {
+            let length = self.data.as_ref().len();
+            if length < 2 {
+                return;
+            }
+
+            for i in 0..(length - 1) {
+                let mut swapped = false;
+
+                for j in 0..(length - i - 1) {
+                    yield j;
+
+                    if self.data.as_ref()[j] > self.data.as_ref()[j + 1] {
+                        self.data.as_mut().swap(j, j + 1);
+                        swapped = true;
+                    }
+                }
+
+                if !swapped {
+                    break;
+                }
+            }
+        }
+    }
+
+    pub fn selection_sort(&mut self) -> impl Iterator<Item = usize> {
+        gen move {
+            let length = self.data.as_ref().len();
+            if length < 2 {
+                return;
+            }
+
+            for i in 0..(length - 1) {
+                let mut min_index = i;
+
+                for j in (i + 1)..length {
+                    yield j;
+
+                    if self.data.as_ref()[j] < self.data.as_ref()[min_index] {
+                        min_index = j;
+                    }
+                }
+
+                self.data.as_mut().swap(i, min_index);
+            }
+        }
+    }
+
+    pub fn insertion_sort(&mut self) -> impl Iterator<Item = usize>
+    where
+        T: Clone,
+    {
+        gen move {
+            let length = self.data.as_ref().len();
+            if length < 2 {
+                return;
+            }
+
+            for i in 1..length {
+                let mut insert_index = i;
+                let current_value = self.data.as_ref()[i].clone();
+
+                for j in (0..i).rev() {
+                    yield j;
+
+                    if self.data.as_ref()[j] > current_value {
+                        self.data.as_mut()[j + 1] = self.data.as_ref()[j].clone();
+                        insert_index = j;
+                    } else {
+                        break;
+                    }
+                }
+
+                self.data.as_mut()[insert_index] = current_value;
+            }
+        }
+    }
+
+    fn merge_sort_inner(data: &mut [T]) -> impl Iterator<Item = usize>
+    where
+        T: Clone,
+    {
+        gen move {
+            let length = data.len();
+            if length < 2 {
+                return;
+            }
+
+            let middle = length / 2;
+            let mut buffer = Box::new(data.to_vec());
+            let (left_half, right_half) = buffer.split_at_mut(middle);
+
+            for index in Box::new(Self::merge_sort_inner(left_half)) {
+                yield index
+            }
+
+            for index in Box::new(Self::merge_sort_inner(right_half)) {
+                yield middle + index
+            }
+
+            let (mut i, mut j, mut k) = (0, 0, 0);
+
+            while i < left_half.len() && j < right_half.len() {
+                yield k;
+
+                if left_half[i] < right_half[j] {
+                    data[k] = left_half[i].clone();
+                    i += 1;
+                } else {
+                    data[k] = right_half[j].clone();
+                    j += 1;
+                }
+
+                k += 1;
+            }
+
+            while i < left_half.len() {
+                yield k;
+
+                data[k] = left_half[i].clone();
+                i += 1;
+                k += 1;
+            }
+
+            while j < right_half.len() {
+                yield k;
+
+                data[k] = right_half[j].clone();
+                j += 1;
+                k += 1;
+            }
+        }
+    }
+
+    pub fn merge_sort(&mut self) -> impl Iterator<Item = usize>
+    where
+        T: Clone,
+    {
+        gen move {
+            for index in Self::merge_sort_inner(self.data.as_mut()) {
+                yield index;
+            }
+        }
+    }
+
+    fn quick_sort_inner(data: &mut [T]) -> impl Iterator<Item = usize>
+    where
+        T: Clone,
+    {
+        gen move {
+            let length = data.len();
+            if length < 2 {
+                return;
+            }
+
+            let pivot = data[length - 1].clone();
+            let mut i = 0;
+
+            for j in 0..(length - 1) {
+                yield j;
+                if data[j] <= pivot {
+                    data.swap(i, j);
+                    i += 1;
+                }
+            }
+
+            data.swap(i, length - 1);
+
+            let (left, right) = data.split_at_mut(i);
+
+            for index in Box::new(Self::quick_sort_inner(left)) {
+                yield index;
+            }
+
+            for index in Box::new(Self::quick_sort_inner(&mut right[1..])) {
+                yield i + index + 1;
+            }
+        }
+    }
+
+    pub fn quick_sort(&mut self) -> impl Iterator<Item = usize>
+    where
+        T: Clone,
+    {
+        gen move {
+            for index in Self::quick_sort_inner(self.data.as_mut()) {
+                yield index;
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::iter;
+
+    macro_rules! test_algorithm {
+        ($algorithm:ident) => {
+            #[test]
+            fn $algorithm() {
+                let mut engine = SortingEngine::from(Vec::new());
+
+                engine.$algorithm().for_each(drop);
+                assert!(engine.data.is_sorted(), "empty case");
+
+                engine.data.push(fastrand::i32(..));
+                engine.$algorithm().for_each(drop);
+                assert!(engine.data.is_sorted(), "single case");
+
+                engine.set_elements(iter::repeat_with(|| fastrand::i32(..)).take(100).collect());
+                engine.$algorithm().for_each(drop);
+                assert!(engine.data.is_sorted(), "100 case");
+
+                engine.$algorithm().for_each(drop);
+                assert!(engine.data.is_sorted(), "sorted case");
+
+                engine.data.reverse();
+                engine.$algorithm().for_each(drop);
+                assert!(engine.data.is_sorted(), "reverse case");
+            }
+        };
+    }
+
+    test_algorithm!(bubble_sort);
+    test_algorithm!(selection_sort);
+    test_algorithm!(insertion_sort);
+    test_algorithm!(merge_sort);
+    test_algorithm!(quick_sort);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,231 +1,46 @@
 #![feature(gen_blocks)]
 
-use std::fmt::Display;
+use std::marker::PhantomData;
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum Algorithm {
-    Bubble,
-    Selection,
-    Insertion,
-    Merge,
-    Quick,
+pub mod algorithms;
+
+#[derive(Debug)]
+pub struct SortingEngine<T, U>
+where
+    T: Ord,
+    U: AsRef<[T]> + AsMut<[T]>,
+{
+    data: U,
+    _marker: PhantomData<T>,
 }
 
-impl Display for Algorithm {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?} Sort", &self)
-    }
-}
-
-pub trait Sortable<T: Ord>: AsRef<[T]> + AsMut<[T]> {
-    fn bubble_sort(&mut self) -> impl Iterator<Item = usize> {
-        gen move {
-            let length = self.as_ref().len();
-            if length < 2 {
-                return;
-            }
-
-            for i in 0..(length - 1) {
-                let mut swapped = false;
-
-                for j in 0..(length - i - 1) {
-                    yield j;
-
-                    if self.as_ref()[j] > self.as_ref()[j + 1] {
-                        self.as_mut().swap(j, j + 1);
-                        swapped = true;
-                    }
-                }
-
-                if !swapped {
-                    break;
-                }
-            }
-        }
+impl<T, U> SortingEngine<T, U>
+where
+    T: Ord,
+    U: AsRef<[T]> + AsMut<[T]>,
+{
+    pub fn get_elements(&self) -> &[T] {
+        self.data.as_ref()
     }
 
-    fn selection_sort(&mut self) -> impl Iterator<Item = usize> {
-        gen move {
-            let length = self.as_ref().len();
-            if length < 2 {
-                return;
-            }
-
-            for i in 0..(length - 1) {
-                let mut min_index = i;
-
-                for j in (i + 1)..length {
-                    yield j;
-
-                    if self.as_ref()[j] < self.as_ref()[min_index] {
-                        min_index = j;
-                    }
-                }
-
-                self.as_mut().swap(i, min_index);
-            }
-        }
+    pub fn set_elements(&mut self, elements: U) {
+        self.data = elements;
     }
 
-    fn insertion_sort(&mut self) -> impl Iterator<Item = usize>
-    where
-        T: Clone,
-    {
-        gen move {
-            let length = self.as_ref().len();
-            if length < 2 {
-                return;
-            }
-
-            for i in 1..length {
-                let mut insert_index = i;
-                let current_value = self.as_ref()[i].clone();
-
-                for j in (0..i).rev() {
-                    yield j;
-
-                    if self.as_ref()[j] > current_value {
-                        self.as_mut()[j + 1] = self.as_ref()[j].clone();
-                        insert_index = j;
-                    } else {
-                        break;
-                    }
-                }
-
-                self.as_mut()[insert_index] = current_value;
-            }
-        }
-    }
-
-    fn merge_sort(&mut self) -> impl Iterator<Item = usize>
-    where
-        T: Clone,
-    {
-        gen move {
-            let length = self.as_ref().len();
-            if length < 2 {
-                return;
-            }
-
-            let middle = length / 2;
-            let mut buffer = Box::new(self.as_ref().to_vec());
-            let (left_half, right_half) = buffer.split_at_mut(middle);
-
-            for index in Box::new(left_half.merge_sort()) {
-                yield index
-            }
-
-            for index in Box::new(right_half.merge_sort()) {
-                yield middle + index
-            }
-
-            let (mut i, mut j, mut k) = (0, 0, 0);
-
-            while i < left_half.len() && j < right_half.len() {
-                yield k;
-
-                if left_half[i] < right_half[j] {
-                    self.as_mut()[k] = left_half[i].clone();
-                    i += 1;
-                } else {
-                    self.as_mut()[k] = right_half[j].clone();
-                    j += 1;
-                }
-
-                k += 1;
-            }
-
-            while i < left_half.len() {
-                yield k;
-
-                self.as_mut()[k] = left_half[i].clone();
-                i += 1;
-                k += 1;
-            }
-
-            while j < right_half.len() {
-                yield k;
-
-                self.as_mut()[k] = right_half[j].clone();
-                j += 1;
-                k += 1;
-            }
-        }
-    }
-
-    fn quick_sort(&mut self) -> impl Iterator<Item = usize>
-    where
-        T: Clone,
-    {
-        gen move {
-            let length = self.as_ref().len();
-            if length < 2 {
-                return;
-            }
-
-            let pivot = self.as_ref()[length - 1].clone();
-            let mut i = 0;
-
-            for j in 0..(length - 1) {
-                yield j;
-                if self.as_ref()[j] <= pivot {
-                    self.as_mut().swap(i, j);
-                    i += 1;
-                }
-            }
-
-            self.as_mut().swap(i, length - 1);
-
-            let (left, right) = self.as_mut().split_at_mut(i);
-
-            for index in Box::new(left.quick_sort()) {
-                yield index;
-            }
-
-            for index in Box::new(right[1..].quick_sort()) {
-                yield i + index + 1;
-            }
-        }
+    pub fn shuffle(&mut self) {
+        fastrand::shuffle(self.data.as_mut());
     }
 }
 
-impl<T: Ord> Sortable<T> for [T] {}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::iter;
-
-    macro_rules! test_algorithm {
-        ($algorithm:ident) => {
-            #[test]
-            fn $algorithm() {
-                let mut numbers = Vec::new();
-
-                numbers.$algorithm().for_each(drop);
-                assert!(numbers.is_sorted(), "empty case");
-
-                numbers.push(fastrand::i32(..));
-                numbers.$algorithm().for_each(drop);
-                assert!(numbers.is_sorted(), "single case");
-
-                numbers = iter::repeat_with(|| fastrand::i32(..)).take(100).collect();
-                numbers.$algorithm().for_each(drop);
-                assert!(numbers.is_sorted(), "100 case");
-
-                numbers.$algorithm().for_each(drop);
-                assert!(numbers.is_sorted(), "sorted case");
-
-                numbers.reverse();
-                numbers.$algorithm().for_each(drop);
-                assert!(numbers.is_sorted(), "reverse case");
-            }
-        };
+impl<T, U> From<U> for SortingEngine<T, U>
+where
+    T: Ord,
+    U: AsRef<[T]> + AsMut<[T]>,
+{
+    fn from(value: U) -> Self {
+        SortingEngine {
+            data: value,
+            _marker: PhantomData,
+        }
     }
-
-    test_algorithm!(bubble_sort);
-    test_algorithm!(selection_sort);
-    test_algorithm!(insertion_sort);
-    test_algorithm!(merge_sort);
-    test_algorithm!(quick_sort);
 }
