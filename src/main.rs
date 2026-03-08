d69f2c881e789616eca27c9d13f152764bc3a99e
Hisham Shefeekh <hishamshefeekh@gmail.com>
2026-03-08 12:06:09 +0530

    add wasm support

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,39 @@
 
 use algorithm_visualizer::AlgorithmVisualizer;
 
+#[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result {
     eframe::run_native(
         "Algorithm Visualizer",
         eframe::NativeOptions::default(),
         Box::new(|cc| Ok(Box::new(AlgorithmVisualizer::new(cc)))),
     )
+}
+
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    use eframe::wasm_bindgen::JsCast as _;
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        let document = web_sys::window()
+            .expect("No window")
+            .document()
+            .expect("No document");
+
+        let canvas = document
+            .get_element_by_id("the_canvas_id")
+            .expect("Failed to find the_canvas_id")
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .expect("the_canvas_id was not a HtmlCanvasElement");
+
+        let _ = eframe::WebRunner::new()
+            .start(
+                canvas,
+                web_options,
+                Box::new(|cc| Ok(Box::new(AlgorithmVisualizer::new(cc)))),
+            )
+            .await;
+    });
 }
